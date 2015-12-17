c2ff7ca8a9ad5293384a1bf342eb3e8c09c51ee8
Daniel M. Sánchez <dmsanchez86@outlook.com>
2015-12-17 15:31:17 +0000

    add language html

--- js/main.js
+++ js/main.js
@@ -79,12 +79,12 @@ function init(){
 	scene.add( new THREE.AmbientLight( 0x443333 ) );
 	
 	var light = new THREE.DirectionalLight( 0xffddcc, 1 );
-			light.position.set( 1, 0.75, 0.5 );
-			scene.add( light );
+	light.position.set( 1, 0.75, 0.5 );
+	scene.add( light );
 
-			var light = new THREE.DirectionalLight( 0xccccff, 1 );
-			light.position.set( -1, 0.75, -0.5 );
-			scene.add( light );
+	var light = new THREE.DirectionalLight( 0xccccff, 1 );
+	light.position.set( -1, 0.75, -0.5 );
+	scene.add( light );
 
 	
 // 	var light = new THREE.DirectionalLight( 0xffddcc, 1 );
